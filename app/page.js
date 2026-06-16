bc7f9752bac87f32171d6509bebeb6063e9166dc
ALLAM BHANU PRAKASH <bunnyyadav504@gmail.com>
2026-06-16 10:55:52 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import Hero from "../components/Hero";
 import About from "../components/About";
 import Projects from "../components/Projects";
 import Skills from "../components/Skills";
+import Certifications from "@/components/Certifications";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 import ParticlesBackground from "../components/ParticlesBackground";
@@ -22,6 +23,8 @@ export default function Home() {
       <Projects />
 
       <Skills />
+
+      <Certifications />
 
       <Contact />
 
